5d7bb8a59e4ee8bfc6847888363a9fad8aa3d625
dphurley <hurley.dannyp@gmail.com>
2018-02-16 21:00:32 -0800

    Fixed Flyway locations, will probably leave this out of the final lesson so we don't have to create separate config for 'dev' and 'test' at the top level.

--- users-api/src/main/resources/dev/db/migration/afterMigrate.sql
+++ users-api/src/main/resources/dev/db/migration/afterMigrate.sql
@@ -1,7 +1,7 @@
--- INSERT INTO USERS
---   (USER_NAME, FIRST_NAME, LAST_NAME)
--- VALUES
---   ('user from seeds', 'blah', 'blah'),
---   ('another user', 'another', 'users'),
---   ('someone', 'ahhhh', 'jflsdkfj'),
---   ('someone else', 'fjfjfjfjf', 'done');
+INSERT INTO USERS
+  (USER_NAME, FIRST_NAME, LAST_NAME)
+VALUES
+  ('user from seeds', 'blah', 'blah'),
+  ('another user', 'another', 'users'),
+  ('someone', 'ahhhh', 'jflsdkfj'),
+  ('someone else', 'fjfjfjfjf', 'done');
